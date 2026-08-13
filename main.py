204fc0350c0636330f6360b978af6e974782fac1
walidsagir@lbert <walidsagir3@gmail.com>
2026-08-13 22:02:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 
 
 
 app = FastAPI()
-
+app.mount("/static",StaticFiles(directory = "static"),name = "static")
 
 @app.get("/")
 def login_page():

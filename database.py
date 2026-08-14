56995d850d54094da71fb4329977282c6db2a80c
walidsagir@lbert <walidsagir3@gmail.com>
2026-08-14 14:48:20 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import Column,String, Integer,create_engine, ForeignKey
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import declarative_base, relationship, session_maker
 import os
 
 
 DATABASE = os.getenv("DATABASE")
 
 engine = create_engine(DATABASE)
+
+sessionLocal = session_maker(bind = engine)
 
 Base = declarative_base()
 
@@ -16,6 +18,13 @@ class Users(Base):
   last_name = Column(String,nullable=False)
   email = Column(String,nullable=False)
   password = Column(String,nullable=False)
+
+def get_db():
+  db = sessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
 
 
 def init():

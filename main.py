00a3f50326a7c23f3f409f14b1c6dd9b93f47973
walidsagir@lbert <walidsagir3@gmail.com>
2026-08-16 05:33:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def signup(details:SignupData, db:Session = Depends(get_db)):
 def logn(details:loginData,request:Request,db:Sesson=Depends(get_db)):
 	email = details.email
 	password  = details.password
-	user= db.query(Users).filter(Users.email == email)
+	user= db.query(Users).filter(Users.email == email).first()
 	if not user:
 		return{"status":"failed","message":"User does not exists please signup"}
 	try:

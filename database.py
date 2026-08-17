0e4c83fb003c3201fcd208453862bdbf63d18753
walidsagir@lbert <walidsagir3@gmail.com>
2026-08-17 08:13:55 +0100

    Update database.py

--- database.py
+++ database.py
@@ -24,19 +24,21 @@ class Users(Base):
   phone_number = Column(String,nullable =False)
   address = Column(String,nullable=False)
   is_organization=Column(Boolean,default=False)
-  organization_name = Column(String,nullable=True)
-  organization_registration =Column(String,nullable=True)
+  agent_id = Column(Integer,ForeignKey ("agents.id"),nullable=True)
+  organization_id = Column(Integer, ForeignKey("organizations.id"),nullable=True)
 
-class Employee(Base):
-    __tablename__ = 'employees'
+class Agents(Base):
+    __tablename__ = 'agents'
     id = Column(Integer, primary_key=True)
-    employee_name = Column(String(255), nullable=False)
-    employee_email = Column(String(255), nullable=False, unique=True)
-    employee_phone_number = Column(String(255), nullable=False)
+    agent_name = Column(String(255), nullable=False)
+    agent_email = Column(String(255), nullable=False, unique=True)
+    agent_phone_number = Column(String(255), nullable=False)
     organization_id = Column(Integer, ForeignKey('organizations.id'))
-    organization = relationship('Organization', back_populates='employees')
+    organization = relationship('Organization', back_populates='agents')
     number_of_assigned_devices = Column(Integer, default=0)
     location = Column(String(255), nullable=True)
+    device_id = Column(Integer, ForeignKey("devices.id"),nullable = True)
+    devices= relationship("Devices",back_populates="agents")
     created_at = Column(DateTime, default=lambda: datetime.now(WAT))
 
 
@@ -47,38 +49,48 @@ class  Organization(Base):
     organization_name = Column(String(255), nullable=False)
     organization_registration = Column(String(255), nullable=False)
     devices = relationship('Device', back_populates='organization')
-    employees  = relationship('Employee', back_populates='organization')
+    agents  = relationship('Agents', back_populates='organization')
     created_at = Column(DateTime, default=lambda: datetime.now(WAT))
 
 class Device(Base):
     __tablename__ = 'devices'
     id = Column(Integer, primary_key=True)
-    device_name = Column(String(255), nullable=False)
-    device_type = Column(String(255), nullable=False)
+    device_model = Column(String(255), nullable=False)
+    device_manufacturer = Column(String(255), nullable=False)
     organization_id = Column(Integer, ForeignKey('organizations.id'))
+    device_type = Column(String(255),nullable = False)
+    serial_number = Column(String(255),nullable=True)
+    
     organization = relationship('Organization', back_populates='devices')
     manufacturer = Column(String(255), nullable=False)
     imei1 = Column(String(255), nullable=False, unique=True)
     imei2 = Column(String(255), nullable=True, unique=True)
     under_maintenance = Column(Boolean, default=False)
-    needs_repair = Column(Boolean, default=False)
+    
     hardware_issues = relationship(
         'HardwareIssue',
         back_populates='device',
         cascade="all, delete-orphan",
     )
-    owners_name = Column(String(255), nullable=False)
-    owners_phone_number = Column(String(255), nullable=False)
-    under_maintainence = Column(Boolean, default=False)
-    needs_repaire = Column(Boolean, default=False)
-    software_issue = Column(Boolean, default=False)
-    hardware_issue = Column(Boolean, default=False)
-    active = Column(Boolean,default=True)
-    new_license = Column(Boolean,default=True)
-    reonboarding = Column(Boolean,default=False)
+    
+    
+    hardware_issues = Column(Boolean, default=False)
     hardware_issue_id = Column(Integer, ForeignKey('hardware_issues.id'), nullable=True)
+    
+    software_issues = relationship(
+        'softwareIssue',
+        back_populates='device',
+        cascade="all, delete-orphan",
+    )
+    software_issue_id = Column(Integer, ForeignKey('software_issues.id'), nullable=True)
+
+    operational = Column(String,default="fully") # we also have options like partially and no
+    active = Column(Boolean,default=True)
+    status = Column(String,default="Registered")
     created_at = Column(DateTime, default=lambda: datetime.now(WAT))
     location = Column(String(255), nullable=True)
+
+
 class HardwareIssue(Base):
     __tablename__ = 'hardware_issues'
     id = Column(Integer, primary_key=True)
@@ -90,9 +102,27 @@ class HardwareIssue(Base):
     scanner = Column(Boolean, default=False)
     touch_screen = Column(Boolean, default=False)
     screen = Column(Boolean, default=False)
-    chargin_point = Column(Boolean, default=False)
+    charging_point = Column(Boolean, default=False)
+    battery = Column(Boolean,default=False)
+    power = Column(Boolean,default=False)
+    camera = Column(Boolean, default=False)
     others = Column(Boolean, default=False)
     description = Column(String(255), nullable=True)
+
+class softwareIssue(Base):
+    __tablename__ = "software_issues"
+    id = Column(Integer, primary_key = True)
+    organization_licenses = Column(Integer,nullable = True)
+    multiple_ids = Column(Boolean, default=False)
+    license_sharing = Column(Boolean, default = False)
+    duplicate_device = Column(Boolean,default = False)
+    authentication_problem = Column(Boolean,default = False)
+    application_error = Column(Boolean,default = False)
+    synchronization_problem = Column(Boolean,default = False)
+    other = Column(String,default = "")
+    description = Column(String,default = "")
+    device = relationship("Devices",back_populates = "software_issues")
+  
 
   
 

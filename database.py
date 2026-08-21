64ee473e5c924447c6070a73e16616de58a45bba
walidsagir@lbert <walidsagir3@gmail.com>
2026-08-21 12:18:28 -0700

    Update database.py

--- database.py
+++ database.py
@@ -80,7 +80,7 @@ class DeviceAgentRequest(Base):
     __tablename__="device_agent_request"
     id = Column(Integer,primary_key=True)
     agent_id = Column(Integer,ForeignKey("agents.id"))
-    organization_id = Column(Integer,ForeignKey("organization.id"))
+    organization_id = Column(Integer,ForeignKey("organizations.id"))
     agent_name = Column(String,nullable=False)
     agent_phone_number=Column(String,nullable=False)
     agent_email = Column(String(255),nullable=True)
